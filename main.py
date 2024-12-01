b853c00437a3d7288b8c9707f0d5c92b3165fa72
Tsumugivolle77 <endsieg77@gmail.com>
2024-12-01 13:45:04 +0100

    update exercise 7. d)

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ user_artist = f"{dat}/user_artist_data_small.txt"
 artist_alias = f"{dat}/artist_alias_small.txt"
 artist_data = f"{dat}/artist_data_small.txt"
 
-ua_dat     = open(user_artist)
-aa_dat = open(artist_alias)
+ua_dat, aa_dat = open(user_artist), open(artist_alias)
 
 ua_lines, aa_lines = ua_dat.read().splitlines(), aa_dat.read().splitlines()
 ua = []
@@ -28,6 +27,17 @@ for line in ua_lines:
 
     ua.append(line)
 
+# Replace artist aliases and aggregate play counts
+ua = []
+for line in ua_lines:
+    line = line.split()
+    artist_id = line[1]
+    # Replace with alias if exists
+    if artist_id in aa.keys():
+        artist_id = aa[artist_id]
+    # Convert to integers and append
+    ua.append([int(line[0]), int(artist_id), int(line[2])])
+
 # %% Start Spark session
 spark = SparkSession.builder\
     .master("local")\
@@ -40,10 +50,15 @@ schema = StructType([
     StructField("playcount", IntegerType(), True),
 ])
 
+# Create a Spark DataFrame
 ua_matrix = spark.createDataFrame(ua, schema)
+ua_merged = (
+    ua_matrix.groupBy("userid", "artistid")
+    .agg(F.sum("playcount").alias("playcount"))
+)
 
 
-# fn for computing Pearson similarity, for 7. b)
+# %% fn for computing Pearson similarity, for 7. b)
 def compute_pearson_similarity(userid1: int, userid2: int, ua_matrix: DataFrame) -> float:
     user1 = (ua_matrix
              .select(["userid", "artistid", "playcount"])
@@ -57,8 +72,8 @@ def compute_pearson_similarity(userid1: int, userid2: int, ua_matrix: DataFrame)
     return user_joined.corr(method='pearson', col1="playcount1", col2="playcount2")
 
 
-# fn for computing knn, for 7. c)
-def compute_knn(user: int, ua_matrix: DataFrame, k) -> list[Row]:
+# %% fn for computing knn, for 7. c)
+def compute_knn(user: int, ua_matrix: DataFrame, k) -> DataFrame:
     others = ua_matrix.select("userid").where(col("userid") != user).distinct()
 
     schema = StructType([
@@ -74,7 +89,7 @@ def compute_knn(user: int, ua_matrix: DataFrame, k) -> list[Row]:
         similarity = spark.createDataFrame([(user, other[0], similarity)], schema)
         similarities = similarities.union(similarity)
 
-    return similarities.dropna(how='any').sort("similarity", ascending=False).take(k)
+    return similarities.dropna(how='any').sort("similarity", ascending=False).limit(k)
 
 
 
@@ -95,20 +110,29 @@ artificial_user = spark.createDataFrame(au_data, schema)
 ua_matrix = ua_matrix.union(artificial_user)
 
 
-# fn for recommend new artist for one user
-def recommend_with_knn(user: int, ua_matrix: DataFrame) -> DataFrame:
-    knn_schema = StructType([
-        StructField("this_userid", IntegerType(), True),
-        StructField("userid", IntegerType(), True),
-        StructField("similarity", FloatType(), True),
-    ])
+# %% fn for recommend new artist for one user
+def recommend_with_knn(user: int, ua_matrix: DataFrame, favcount: int=5, k: int=100) -> DataFrame:
+    fav_artists_schema = StructType([StructField("artistid", IntegerType(), True)])
 
-    knn = spark.createDataFrame(compute_knn(user, ua_matrix, k=10), knn_schema)
+    # get the k nearest neighbour id
+    knn = compute_knn(user, ua_matrix, k=k).select("other_userid")
+    fav_artists = spark.createDataFrame([], fav_artists_schema)
+
+    # traverse the neighbourhood
+    for neighbour in knn.rdd.collect():
+        # find the top 'favcount' liked artist of the neighbour
+        new_fav_artists = (ua_matrix
+                            .select("artistid")
+                            .where(col("userid") == neighbour[0])
+                            .sort("playcount", ascending=False)
+                           .limit(favcount))
+        fav_artists = fav_artists.union(new_fav_artists)
+
     old_artists = ua_matrix.select("artistid").where(col("userid") == user).distinct()
-    new_artists = knn.join(ua_matrix, on="userid", how="inner").select(["artistid"]).distinct()
-    # new_artists = new_artists.where(~F.col("artistid").isin(old_artists))
+    fav_artists = fav_artists.subtract(old_artists)
 
-    return new_artists
+    return fav_artists.distinct()
+
 
 # %% test recommend with knn
-recommend_with_knn(uid, ua_matrix).show()
+recommend_with_knn(uid, ua_matrix).show(50)
